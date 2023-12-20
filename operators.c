5174cc9bfaaedab25275742091e1369e83c44969
JKariithi <147997444+JKariithi@users.noreply.github.com>
2023-12-20 11:49:50 +0300

    Importance of parentheses placement
    This code explains how parentheses over ride the priority of the operators, This means that whatever is in the parentheses is completed first

---
+++ operators.c
@@ -0,0 +1,17 @@
+// The code below shows how we use multiple operators in a given function
+// We also us parenthesis to our advantage 
+// The original was double ans = 10.0 + 2.0 /3.0 - 2.0 * 2.0;
+// by use of parenthesis our value is now 11
+
+
+#include <stdio.h>
+
+int main(){
+    double ans = 10.0 + 2.0/((3.0 - 2.0 )*2.0);
+
+    printf("answer is : %lf", ans);
+
+    return 0;
+}
+// challenge:
+//  rearrange the parenthesis to give a value of 8.
