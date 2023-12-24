479840792450a0a536c38e3be4da1e7aa903ed13
janice2222 <janicekariithi@gmail.com>
2023-12-24 22:12:48 +0300

    Increament Operator

---
+++ lecture 1/unary.c
@@ -0,0 +1,16 @@
+// Today I was learning about unary operators most especially increament an decreament operators
+
+#include <stdio.h>
+
+int main(){
+
+    int y = 11;
+    int x = 12;
+    y = x ++ ;
+    printf("%d ",x);
+    
+    printf("%d ",y);
+
+    return 0;
+}
+// I will do more examples to make sure I undersatand.
