fde62e5523bd09558570252189a9bffcfc125989
Janice Kariithi <147997444+JKariithi@users.noreply.github.com>
2023-12-26 23:45:26 +0300

    Checking if year is a leap year or a regular year
    This code uses Terniary operations

---
+++ challenges/leap.c
@@ -0,0 +1,24 @@
+
+//This code was to check if a year is a leap year or a common year using conditions
+
+#include <stdio.h>
+int main()
+{
+  int year;
+
+  printf("Enter year ");
+  scanf("%d",&year);
+    /*
+     * If year%4==0 and year%100==0 then
+     *     print leap year
+     * else if year%400==0 then
+     *     print leap year
+     * else
+     *     print common year 
+     */
+    (year%4==0 && year%100!=0) ? printf("LEAP YEAR") :
+        (year%400 ==0 ) ? printf("LEAP YEAR") : printf("COMMON YEAR");  
+        
+    return 0;
+//What I learn is how to use mod operator.
+}
