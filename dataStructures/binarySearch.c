d00f20a4143b9b7cd09ad0fe1a4174fde1c3691b
JKariithi <kariithijanice@gmail.com>
2024-01-04 20:06:27 +0300

    on hold

---
+++ dataStructures/binarySearch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//First I am putting this file on hold till grasp basics
+//here is an array of a certain number of integers 
+//this program determines the number in the certain n position.
+
+int main (){
+
+ 
+ int arr[] = {1,2,3,5,6,7,9};
+ printf("Element arr[2] %d \n", arr[2]);
+
+ return 0;
+}
