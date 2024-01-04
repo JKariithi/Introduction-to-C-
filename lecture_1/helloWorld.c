d3e09ad3962ee9fe35c3f67cd558339b0cde3960
JKariithi <kariithijanice@gmail.com>
2024-01-04 19:16:38 +0300

    Hello World

---
+++ lecture_1/helloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+
+    printf("Hello World \n");
+    return 0;
+}
+// I had to guys the basic and or first program in C or any programming language.
+// to print this on the console 
+// cd into the folder intended example mine is lecture_1
+//gcc -o  <foldername> <filename>
+//then
+//./<foldername>
+// using my example it looks like this 
+//then 
+//gcc -o lecture_1 helloWorld.c
+// ./lecture_1 
+//output will then be printed out
