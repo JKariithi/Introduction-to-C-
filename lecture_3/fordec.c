93180726d04a7363fff6ffd91458ccccdc64a52c
JKariithi <kariithijanice@gmail.com>
2024-01-08 16:27:38 +0300

    decreament of values

---
+++ lecture_3/fordec.c
@@ -0,0 +1,8 @@
+//showing how to decreament values using a for loop 
+#include <stdio.h>
+void main(){
+    int i=10;
+    for (;i>0;i--)
+     printf("%d \n",i);
+//if i is greater than 0 print .
+}
