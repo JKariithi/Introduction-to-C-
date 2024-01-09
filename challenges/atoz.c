6d361384c954b87061005bbf6c6d6b16897e097b
JKariithi <kariithijanice@gmail.com>
2024-01-09 10:48:05 +0300

    printing a to z using characters

---
+++ challenges/atoz.c
@@ -0,0 +1,11 @@
+// A program to print a to z using while loop
+#include <stdio.h>
+void main (){
+    char ch = 'a';
+    printf("learn your abc's below \n\n");
+    while (ch <='z'){
+        printf("%c \n",ch) ;
+        ch++;
+    }
+
+}
