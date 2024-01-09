33b27b627af4b24bc752c0a7f4d893effff6a15e
JKariithi <kariithijanice@gmail.com>
2024-01-09 10:21:26 +0300

    while loop syntax

---
+++ lecture_3/while.c
@@ -0,0 +1,18 @@
+//while loop is an entry control loop 
+/*
+intialization
+while(Termination condition){
+    statements
+
+    update expression comes here ,
+}
+*/ 
+// program to print 1 to 10 
+#include <stdio.h>
+void main(){
+    int i =1 ;
+    while(i<=10){
+       printf("%d \n",i);
+       i++;
+    }
+}
