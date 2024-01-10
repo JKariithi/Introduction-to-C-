63b1788f79745ed73491ecf4112932327b8edf58
JKariithi <kariithijanice@gmail.com>
2024-01-10 18:27:08 +0300

    hcf of two numbers

---
+++ challenges/hcf.c
@@ -0,0 +1,25 @@
+// find the hcf of two numbers 
+#include <stdio.h>
+void main (){
+   int num1,num2,i,hcf = 1,highest;
+  printf("Enter first number: ");
+  scanf("%d",&num1);
+  printf("Enter second number: ");
+  scanf("%d",&num2);
+  if (num1 > num2)
+    highest = num1;
+ else 
+    highest = num2 ;
+  for(i=1; i < highest;i++){
+   if ( num1 % i == 0 && num2 % i == 0){
+      printf("%d\n", hcf);
+      hcf *= i;
+      num1 /= i;
+      num2 /= i;
+   }
+
+
+  }
+
+printf("hcf = %d \n", hcf);
+}
