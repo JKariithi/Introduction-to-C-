a1cad5c41afe4550973f7f485d5d2ffe90252064
JKariithi <kariithijanice@gmail.com>
2024-01-10 17:21:48 +0300

    do while loop

---
+++ lecture_3/dowhile.c
@@ -0,0 +1,15 @@
+// Do while loop 
+// Exit control loop 
+#include <stdio.h>
+int main (){
+
+    printf("Jerry\n");
+    int i = 1;
+   do { // Do this 
+    printf(" i = %d \n",i);
+    i++;
+    
+   } while(i<=10); //While this condition is true 
+
+    return 0;
+}
