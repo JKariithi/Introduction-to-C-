e400c3714a4a286107d6650309281d49343e6aeb
JKariithi <kariithijanice@gmail.com>
2024-01-12 12:14:26 +0300

    added a function

--- lecture_3/break.c
+++ lecture_3/break.c
@@ -1,12 +1,18 @@
+
+
 #include <stdio.h>
 int main (){
-    int i;
+    int i,a, sum = 0;
     for (i=1;i <= 5;i++){
-        if (i < 0){
-            break;
-        }
         printf("Enter number: ");
-        scanf("%d",&i);
-    }
+        scanf("%d",&a);
+        if (a < 0){
+            puts("Exits loop");
+            break; 
+        }
+        
+    sum = sum + a;}
+
+    printf("sum = %d \n",sum);
     return 0;
 }
