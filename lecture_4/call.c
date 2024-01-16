c6741965bc89c1ede6695d40411ec6b7208abd0e
JKariithi <kariithijanice@gmail.com>
2024-01-16 21:39:11 +0300

    fixing declaration

--- lecture_4/call.c
+++ lecture_4/call.c
@@ -1,7 +1,9 @@
 // Call by reference 
 //pointer neeeded in declaration also 
+//The declaration parameters and that of the definition should be the same 
+
 #include <stdio.h>
-int val(int*, );
+int val(int*, int*);
 
 void main(){
     int a = 50, b = 70;
