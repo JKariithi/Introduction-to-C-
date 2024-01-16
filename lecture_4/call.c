ef224315092f8a587d2f49a23a26848afcdfd589
JKariithi <kariithijanice@gmail.com>
2024-01-16 21:31:59 +0300

    Call by refference

---
+++ lecture_4/call.c
@@ -0,0 +1,17 @@
+// Call by reference 
+
+#include <stdio.h>
+int val();
+
+void main(){
+    int a = 50, b = 70;
+    val(&a, &b);
+    printf("a = %d and b = %d \n",a,b);
+}
+int  val (int *a,int *b){
+    
+    *a = 5;
+    *b = 7;
+ 
+    printf("a = %d and b = %d \n",*a,*b);
+}
