199cda9a4a7df7e3b13aae6bdd4c80fa96019841
JKariithi <kariithijanice@gmail.com>
2024-02-10 18:54:42 +0300

    converting degrees fahrenheit to celsius using a function conversion

---
+++ challenges/conversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int conversion(int lower, int upper, int step);
+int main(){
+    int fahr , celsius ;
+    fahr = 0;
+    celsius = conversion(fahr, 300, 20);
+    printf("The table of conversion of Farenheit to degree celsius is as shown above \n\n");
+    
+    return 0;
+
+}
+int conversion(int lower, int upper, int step){
+    int fahr,celsius;
+    fahr = lower ;
+    while  (fahr <= upper ){
+        celsius = 5*(fahr - 20)/9;
+         printf("%d\t%d\n",fahr,celsius);
+        fahr += 20;
+    }
+
+    return celsius;
+}
