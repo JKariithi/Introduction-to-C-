ae6a03fb45d04618b5781af03348898c1669ae5d
JKariithi <kariithijanice@gmail.com>
2024-02-11 10:33:41 +0300

    adding strings with a limit of 10 characters and getting length of a

--- lecture_4/arrays.c
+++ lecture_4/arrays.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h> 
 #define max 10
 int main(){
     char a[max],b[max],c[max];
+    int length = 0;
 
     printf("Input 3 words \n");
     scanf("%s %s %s",a,b,c); 
     printf("a = %s, b =%s , c =%s \n",a,b,c);
+    length = strlen(a);
+    printf("length of a %d",length);
     return 0;
 
 
