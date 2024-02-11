cd2dbfe9f53ce0c92afcb17ef9e607360b5b5852
JKariithi <kariithijanice@gmail.com>
2024-02-11 21:02:33 +0300

    hardcoding it without use of automated function

--- lecture_4/arrays.c
+++ lecture_4/arrays.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <string.h> 
+
 #define max 10
 int main(){
     char a[max],b[max],c[max];
-    int length = 0;
+  
 
     printf("Input 3 words \n");
     scanf("%s %s %s",a,b,c); 
     printf("a = %s, b =%s , c =%s \n",a,b,c);
-    length = strlen(a);
+    int length = 0;
+    while (a[length] != '\0')
+    length++;
+  
     printf("length of a %d",length);
     return 0;
-
+   
 
 }
