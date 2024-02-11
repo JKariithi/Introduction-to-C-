0cd3477fcb30c6101942b2ea6d647d10a9ba131b
JKariithi <kariithijanice@gmail.com>
2024-02-11 10:26:20 +0300

    adding strings with a linit of 10 characters

---
+++ lecture_4/arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define max 10
+int main(){
+    char a[max],b[max],c[max];
+
+    printf("Input 3 words \n");
+    scanf("%s %s %s",a,b,c); 
+    printf("a = %s, b =%s , c =%s \n",a,b,c);
+    return 0;
+
+
+}
