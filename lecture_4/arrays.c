c1be3f79413a9e60466c31b24dd76f562cd57f32
JKariithi <kariithijanice@gmail.com>
2024-02-17 20:44:53 +0300

    passing arrays in functions

--- lecture_4/arrays.c
+++ lecture_4/arrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void add(int array[]);
 
 void main (){
     int fruits[] = {2,4,6,7,8,10,545};
@@ -6,4 +7,13 @@ void main (){
     for (; i < 7 ; i++){
        printf("%d \n",fruits[i]);
     }
+    add(fruits);
+    printf("%d\n",fruits[0]);
+    printf("%p\n",fruits);
 }
+void add(int array[]){
+    printf("%p\n",array);
+    array [0] = array [0] + 1;
+
+}
+//We can see that the addres of array and fruits is the same without use of pointers.
