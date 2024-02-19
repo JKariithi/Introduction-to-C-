a637df25711ae583f35d73622671a60996ecb5cb
JKariithi <kariithijanice@gmail.com>
2024-02-19 10:49:28 +0300

    using a keyword quit to stop a loop

--- problem3.2/main.c
+++ problem3.2/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-// #include "dict.c"
+#include <string.h>
+// the keyword strcmp is from the library string.h it is used to compar the word inputed and the word "quit"
+// if the are equal it returns 0 if not anyother number is given
+
 int main(void){
+
     char word[200];
-    printf("Enter a  word: ");
-    scanf("%s",word);
-    printf("word : %s \n ", word);
-  
-   
+    while(strcmp(word, "quit") != 0){
+        printf("Enter a word (use the keyword quit to exit the loop): ");
+        scanf("%s", word);
+        printf("word: %s \n", word);
+    }
+
     return 0;
 
 }
