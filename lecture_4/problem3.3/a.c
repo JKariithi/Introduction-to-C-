547261c9cb2b618ad734ddbea4de6a5aa2cc7259
JKariithi <kariithijanice@gmail.com>
2024-02-19 21:05:19 +0300

    factorials using while loop

--- lecture_4/problem3.3/a.c
+++ lecture_4/problem3.3/a.c
@@ -1,15 +1,16 @@
  #include <stdio.h>
 int factorial(int); 
  int main(){
-    factorial(5);
+    factorial(9);
     return 0;
  }
  int factorial(int n) {
- int i , ret = 1 ;
- for( i = 1 ; i <= n ; i ++){
-    printf("%d!",i);
+ int i = 1, ret = 1 ;
+ while ( i <= n ){
     ret *= i ;
+    printf("%d!\n",i);
+    i ++; 
  }
- printf("Factorial = %d \n",ret);
+ printf("Factorial = %d  \n",ret);
 return ret ;
 }
