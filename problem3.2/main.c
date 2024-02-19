f58c89347b325cf5902f0e86ac7e4c507fea5695
JKariithi <kariithijanice@gmail.com>
2024-02-19 10:39:49 +0300

    input of strings and printin them out

--- problem3.2/main.c
+++ problem3.2/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include "dict.c"
-int sum(int *,int *);
-int main(){
-    int a = 5,b = 6;
-    sum(&a,&b);
+// #include "dict.c"
+int main(void){
+    char word[200];
+    printf("Enter a  word: ");
+    scanf("%s",word);
+    printf("word : %s \n ", word);
+  
    
+    return 0;
+
 }
 
